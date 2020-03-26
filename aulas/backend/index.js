a650f4ef6298298eac3eedd11f02bf82ad04ba7b
Maitê Caetano <maite@MacBook-Air-de-Maite.local>
2020-03-26 19:13:03 -0300

    criando o index.js e a rota de execução da aplicação

---
+++ aulas/backend/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+// requerendo o módulo express para este arquivo index.js
+
+const app = express();
+// armazenando a apliação na variável app
+
+app.listen(3333);
+// variável app ouvindo a rota localhost:3333
+
