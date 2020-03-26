bd5b8c609b10d4e58e932cdec75801d22a8ee87b
Maitê Caetano <maite@MacBook-Air-de-Maite.local>
2020-03-26 20:10:20 -0300

    fim da estrutura node com o app.get da rota configurado

--- aulas/backend/index.js
+++ aulas/backend/index.js
@@ -4,6 +4,16 @@ const express = require('express');
 const app = express();
 // armazenando a apliação na variável app
 
+app.get('/', (request, response) => {
+    return response.json({
+        evento: "Semana Omnistack 11.0",
+        aluna: "Maitê Marques Caetano"
+    });
+});
+// ciando a rota raiz da aplicação com a '/''
+// passando uma função que recebe como parâmetros a 
+// requisição (request) e a resposta (response)
+
 app.listen(3333);
-// variável app ouvindo a rota localhost:3333
+// variável app ouvindo a rota localhost:3333 | não existe rota
 
